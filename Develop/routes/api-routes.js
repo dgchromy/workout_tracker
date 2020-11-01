ea5dc1d07ae56ab08d81df9f10844292d25d8a14
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-11-01 14:21:21 -0800

    finished apir routes.json created some console logs for when server.js is created

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -18,6 +18,37 @@ module.exports = function(app) {
     });
 
     app.put("/api/workouts/:id", (req,res) => {
-        dbWorkout.findOneUpdate({ _id: req.params.id },)
-    })
+        dbWorkout.findOneUpdate({ _id: req.params.id },{
+            $insc: { totalDuration: req.body.duration},
+            $push: { exercises: req.body }
+        }, { new: true}). then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+
+    // adding new workout
+    app.post("/api/workouts", ({ body }, res) => {
+     
+        db.Workout.create(body).then((dbWorkout => {
+            res.json(dbWorkout);
+        })).catch(err => {
+            res.json(err);
+        });
+    });
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        console.log("ALL workouts")
+        console.log(dbWorkout);
+      res.json(dbWorkout);  
+    }).catch(err => {
+        res.json(err);
+    });
+
+});
+
+
 }
