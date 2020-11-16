d325d48bffcfda397d7d2ca4dba92c0dafb4c78a
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-11-16 15:17:14 -0800

    seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/workoutDB", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
